dd5dbdfe74e1e3bb90246fa7830b0ef36e353877
asv0r <cdscott9@gmail.com>
2016-05-16 20:42:39 -0400

    Initial commit

---
+++ src/com/risserigdon/algorithms/DirectedAcyclicGraph.java
@@ -0,0 +1,20 @@
+package com.risserigdon.algorithms;
+
+public class DirectedAcyclicGraph {
+
+    /**
+     * Add an edge from {@code source} to {@code destination} and return
+     * {@code true} if the edge is added as a result of this function. If
+     * either {@code source} or {@code destination} does not exist, create
+     * them before adding the edge.
+
+     * @param source the name of the source node
+     * @param destination the name of the destination edge
+     * @return {@code true} if the edge is added as a result of this 
+     * method call
+     */
+    public boolean addEdge(String source, String destination){
+
+    }
+
+}
