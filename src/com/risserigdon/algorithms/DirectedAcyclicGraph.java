9bde85e465db0862f719332d3ba7e05061b2b2b0
risu-kun <risse.rigdon@gmail.com>
2016-05-16 23:03:08 -0400

    Change indentation to spaces
    Eclipse settings and GitHub weren't playing nicely together.

--- src/com/risserigdon/algorithms/DirectedAcyclicGraph.java
+++ src/com/risserigdon/algorithms/DirectedAcyclicGraph.java
@@ -3,60 +3,60 @@ package com.risserigdon.algorithms;
 import java.util.ArrayList;
 
 public class DirectedAcyclicGraph {
-	
-	/**
-	 * Represents a single node of a directed acyclic graph
-	 */
-	public static class DAGNode {
-		ArrayList<DAGNode> children;
-		String name;
-		
-		public DAGNode(String _name){
-			children = new ArrayList<DAGNode>();
-			name = _name;
-		}
-		
-		/**
-		 * Add a node to this node's children list.
-		 * 
-		 * @param child The node to be added
-		 */
-		public void addChild(DAGNode child){
-			children.add(child);
-		}
-		
-		/**
-	     * Depth-first search through the graph for the target String
-	     * 
-	     * @param target the String to be found in the name field of a node
-	     * @return the node where the target is found, or null if it is not
-	     */
-	    public DAGNode find(String target){
-	    	if (name == target)
-	    		return this;
-	    	else if (children.isEmpty())
-	    		return null;
-	    	else {
-	    		DAGNode found = null;
-	    		int i = 0;
-	    		while (found == null && i < children.size()) {
-	    			found = children.get(i).find(target);
-	    			i++;
-	    		}
-	    		return found;
-	    	}
-	    		
-	    }
-	    
-	    public String getName(){
-	    	return name;
-	    }
-	    		
-		public ArrayList<DAGNode> getChildren() {
-			return children;
-		}
 
-	}
+    /**
+     * Represents a single node of a directed acyclic graph
+     */
+    public static class DAGNode {
+        ArrayList<DAGNode> children;
+        String name;
+
+        public DAGNode(String _name){
+            children = new ArrayList<DAGNode>();
+            name = _name;
+        }
+
+        /**
+         * Add a node to this node's children list.
+         * 
+         * @param child The node to be added
+         */
+        public void addChild(DAGNode child){
+            children.add(child);
+        }
+
+        /**
+         * Depth-first search through the graph for the target String
+         * 
+         * @param target the String to be found in the name field of a node
+         * @return the node where the target is found, or null if it is not
+         */
+        public DAGNode find(String target){
+            if (name == target)
+                return this;
+            else if (children.isEmpty())
+                return null;
+            else {
+                DAGNode found = null;
+                int i = 0;
+                while (found == null && i < children.size()) {
+                    found = children.get(i).find(target);
+                    i++;
+                }
+                return found;
+            }
+
+        }
+
+        public String getName(){
+            return name;
+        }
+
+        public ArrayList<DAGNode> getChildren() {
+            return children;
+        }
+
+    }
 
     /**
      * Add an edge from {@code source} to {@code destination} and return
@@ -70,44 +70,44 @@ public class DirectedAcyclicGraph {
      * method call
      */
     public boolean addEdge(String source, String destination){
-		//search for source
-    	//if source is not found, create a new node
-    	//search for destination
-    	//if destination is not found, create a new node
-    	
-    	//add destination to the source's children list
-    	return false;
+        //search for source
+        //if source is not found, create a new node
+        //search for destination
+        //if destination is not found, create a new node
+
+        //add destination to the source's children list
+        return false;
     }
-    
+
     public static void runTests(){
-    	DAGNode root = new DAGNode("root node");
-    	DAGNode firstChild = new DAGNode("root's first child");
-    	DAGNode secondChild = new DAGNode("root's second child");
-    	DAGNode thirdChild = new DAGNode("root's third child");
-    	
-    	root.addChild(firstChild);
-    	root.addChild(secondChild);
-    	root.addChild(thirdChild);
-    	
-    	DAGNode firstGrandchild = new DAGNode("firstChild's child");
-    	firstChild.addChild(firstGrandchild);
-    	
-    	DAGNode firstGreatGrandchild = new DAGNode("firstChild's grandchild");
-    	firstGrandchild.addChild(firstGreatGrandchild);
-    	
-    	
-    	// print True if the third child was added successfully to the root
-    	System.out.println(root.find("root's third child") != null);
-    	
-    	// print True if the grandchild was added successfully to the first child
-    	System.out.println(root.find("firstChild's child") != null);
-    	
-    	// print True if the grandchild was added successfully to the first child
-    	System.out.println(root.find("firstChild's grandchild") != null);
+        DAGNode root = new DAGNode("root node");
+        DAGNode firstChild = new DAGNode("root's first child");
+        DAGNode secondChild = new DAGNode("root's second child");
+        DAGNode thirdChild = new DAGNode("root's third child");
+
+        root.addChild(firstChild);
+        root.addChild(secondChild);
+        root.addChild(thirdChild);
+
+        DAGNode firstGrandchild = new DAGNode("firstChild's child");
+        firstChild.addChild(firstGrandchild);
+
+        DAGNode firstGreatGrandchild = new DAGNode("firstChild's grandchild");
+        firstGrandchild.addChild(firstGreatGrandchild);
+
+
+        // print True if the third child was added successfully to the root
+        System.out.println(root.find("root's third child") != null);
+
+        // print True if the grandchild was added successfully to the first child
+        System.out.println(root.find("firstChild's child") != null);
+
+        // print True if the grandchild was added successfully to the first child
+        System.out.println(root.find("firstChild's grandchild") != null);
     }
-    
+
     public static void main(String args[]){
-    	runTests();	
+        runTests();       	
     }
 
 }
