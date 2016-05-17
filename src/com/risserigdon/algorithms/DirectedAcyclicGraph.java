e018940b4224b1978639947df0b7afca51dda5f2
risu-kun <risse.rigdon@gmail.com>
2016-05-16 21:24:07 -0400

    Allow creation of nodes in the graph

--- src/com/risserigdon/algorithms/DirectedAcyclicGraph.java
+++ src/com/risserigdon/algorithms/DirectedAcyclicGraph.java
@@ -1,6 +1,35 @@
 package com.risserigdon.algorithms;
 
+import java.util.ArrayList;
+
 public class DirectedAcyclicGraph {
+	
+	/**
+	 * Represents a single node of a directed acyclic graph
+	 */
+	public static class DAGNode {
+		ArrayList<DAGNode> children;
+		String name;
+		
+		public DAGNode(String _name){
+			children = new ArrayList<DAGNode>();
+			name = _name;
+		}
+		
+		/**
+		 * Add a node to this node's children list.
+		 * 
+		 * @param child The node to be added
+		 */
+		public void addChild(DAGNode child){
+			children.add(child);
+		}
+		
+		public ArrayList<DAGNode> getChildren() {
+			return children;
+		}
+
+	}
 
     /**
      * Add an edge from {@code source} to {@code destination} and return
@@ -14,7 +43,22 @@ public class DirectedAcyclicGraph {
      * method call
      */
     public boolean addEdge(String source, String destination){
-
+		//search for source
+    	//if source is not found, create a new node
+    	//search for destination
+    	//if destination is not found, create a new node
+    	
+    	//add destination to the source's children list
+    	return false;
+    }
+    
+    public static void main(String args[]){
+    	DAGNode root = new DAGNode("test parent");
+    	DAGNode child = new DAGNode("test child");
+    	root.addChild(child);
+    	
+    	// print True if the test child was added successfully to the root
+    	System.out.println(root.getChildren().contains(child));
     }
 
 }
